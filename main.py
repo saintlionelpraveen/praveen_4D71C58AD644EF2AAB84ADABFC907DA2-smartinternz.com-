c315d5373253757128a47ce7d864bfc3350931e1
saintlionelpraveen <145955133+saintlionelpraveen@users.noreply.github.com>
2023-10-12 10:47:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-def main():
-    while True:
-        try:
-            num = int(input("Enter a non-negative integer: "))
-            if num < 0:
-                raise ValueError("The number must be non-negative.")
-            break
-        except ValueError as e:
-            print("Invalid input:", e)
-    
-    print("The factorial of", num, "is", factorial(num))
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited ${amount}. New balance: ${self.__account_balance:.2f}"
+        else:
+            return "Invalid deposit amount. Amount must be greater than zero."
 
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.__account_balance:.2f}"
+        elif amount <= 0:
+            return "Invalid withdrawal amount. Amount must be greater than zero."
+        else:
+            return "Insufficient funds for withdrawal."
+
+    def display_balance(self):
+        return f"Account balance for {self.__account_holder_name}: ${self.__account_balance:.2f}"
+
+# Example usage:
 if __name__ == "__main__":
-    main()
+    # Create an instance of the BankAccount class
+    account = BankAccount("123456789", "John Doe", 1000.0)
+
+    # Deposit money
+    print(account.deposit(500))
+    print(account.deposit(0))  # Should print an error message
+    print(account.display_balance())
+
+    # Withdraw money
+    print(account.withdraw(200))
+    print(account.withdraw(1500))  # Should print an error message
+    print(account.display_balance())
